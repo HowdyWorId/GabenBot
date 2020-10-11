0c59264ae30d7ce036b715e08849b4019b573be6
HowdyWorId <69632436+HowdyWorId@users.noreply.github.com>
2020-10-11 12:58:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Python 3.7.3
-# https://discordapp.com/oauth2/authorize?&client_id=702107949421822104&scope=bot&permissions=2147483639
 import logging
 import os
 import random
@@ -21,45 +19,58 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='.')
 bot.remove_command('help')
 
-async def background_task():
-    urls = ['https://vk.com/oryslenti','https://vk.com/public171746659', 'https://vk.com/fails66','https://vk.com/nestandartniememi','https://vk.com/willscary','https://vk.com/chlgpro','https://vk.com/shkolkrim']
-    def meme_generator(urls: list = urls):
-        lst = []
-        for url in urls:
-            r = requests.get(url)  # /photo-171746659_457350338?list=wall-171746659_1801160&amp
+class Meme:
+    def __init__(self, urls=['https://vk.com/oryslenti', 'https://vk.com/public171746659',
+        'https://vk.com/nestandartniememi', 'https://vk.com/chlgpro',
+        'https://vk.com/shkolkrim']):
+        self.__urls = urls
+        self.__memes = self.meme_generator()
+        # print(self.__memes)
+
+
+    def meme_generator(self):
+        d = []
+        for url in self.__urls:
+            r = requests.get(url)
             soup = BeautifulSoup(r.content, 'html.parser',
-                                 parse_only=SoupStrainer('div'))  # ,parse_only=SoupStrainer('a')
-            # print(soup)
+                                 parse_only=SoupStrainer('div'))
 
+            convert = soup.find_all('div', {'class': "wi_body wi_no_text"})
             try:
-                convert = soup.find_all('div', {'class': "wi_body wi_no_text"})
-                if convert == []:
-                    print(f'cant parse to {url}')
-                    raise IndexError
-                for el in convert:
-                    jpg = [i[1:-1] for i in re.findall(r'url(.*?);', str(el))]
-                    lst.append(['', jpg])
+                for el in convert[1:]:
+                    rel = re.findall(r'url(.*?);', str(el))
+                    if len(rel) == 1:
+                        d.append({'public':url, 'meme':['',rel[0][1:-1]],})
+            except Exception:
+                print(f'cant parse to {url}')
+                continue
+        return d
 
-            except IndexError:
-                convert = soup.find_all('div', {'class': 'wi_body'})
-                for el in convert:
-                    jpg = [i[1:-1] for i in re.findall(r'url(.*?);', str(el))]
-                    text = re.findall(r'<div class="pi_text">(.*?)</div>', str(el))[0]
-                    if len(jpg) < 2:
-                        lst.append([text, jpg])
+    @property
+    def publics(self):
+        return self.__urls
 
-        return lst
+    @property
+    def memes(self):
+        return self.__memes
 
+    @staticmethod
+    def new_urls(urls):
+        return Meme(urls=urls)
+
+
+async def background_task():
+    m = Meme().memes
     await bot.wait_until_ready()
-    channel = bot.get_channel(701437053593714720)
+    # channel = bot.get_channel(701437053593714720)
+    channel = bot.get_channel(761600162501754910)
     if bot.is_closed:
-        memes = meme_generator()
-        memes = [ memes[random.randint(0,len(memes)-1)] for _ in range(0,5) ]
-        # memes = list(set(memes))
-        # print(memes)
+        r = random.randint(0,len(m)-1)
+        memes = [m[r]['meme']]
+        print(memes)
         for meme in memes:
-            await channel.send(f'{meme[0]}\n {" ".join(meme[1])}')
-        await asyncio.sleep(36000)
+            await channel.send(f'{meme[0]}\n {"".join(meme[1])}')
+        await asyncio.sleep(random.randint(1000,40000))
         await background_task()
     else:
         print('bot is closed')
@@ -114,6 +125,6 @@ async def clear(ctx, amount=10):
         await ctx.channel.send('{0.author.mention} Иди нах :)'.format(ctx.message))
 
 
-bot.loop.create_task(background_task())
+meme_task = bot.loop.create_task(background_task())
 token = os.environ.get('BOT_TOKEN')
 bot.run(token)
